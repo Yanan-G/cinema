b41af3832b6b7446b21f5bcff1963cc4b7840349
YananGeng <yanan@padmatch.com>
2021-02-23 19:52:05 -0500

    added header component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { Provider } from 'react-redux';
 
-import './App.css';
+import './App.scss';
+import Header from './components/header/Header';
 import store from './redux/store';
 
 function App() {
   return (
     <Provider store={store}>
+      <Header />
       <h1>Hello</h1>
     </Provider>
   );
