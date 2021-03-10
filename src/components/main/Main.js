074b4a093ec0094dbfa327c267252c88f19b4c18
YananGeng <yanan@padmatch.com>
2021-03-09 21:54:45 -0500

    added details component

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -60,17 +60,21 @@ const Main = (props) => {
   return (
     <>
       <div className="main" ref={mainRef} onScroll={handleScroll}>
-        {loading ? (
+        {loading
+          ? (
           <Spinner />
-        ) : (
+            )
+          : (
           <>
-            {searchResult && searchResult.length === 0 ? (
+            {searchResult && searchResult.length === 0
+              ? (
               <MainContent />
-            ) : (
+                )
+              : (
               <SearchResult />
-            )}
+                )}
           </>
-        )}
+            )}
         <div ref={bottomLineRef}></div>
       </div>
     </>
